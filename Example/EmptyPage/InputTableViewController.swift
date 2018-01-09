58913783509af81cb08b9b5ca3199ed25cbcbb82
linhey <linhan.linhey@outlook.com>
2018-01-09 22:23:09 +0800

    init

---
+++ Example/EmptyPage/InputTableViewController.swift
@@ -0,0 +1,19 @@
+//
+//  InputTableViewController.swift
+//  EmptyPage_Example
+//
+//  Created by bigl on 2018/1/9.
+//  Copyright © 2018年 CocoaPods. All rights reserved.
+//
+
+import UIKit
+import EmptyPage
+
+class InputTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+      EmptyPage.begin()
+    }
+
+}
