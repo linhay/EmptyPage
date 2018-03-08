53c0a2ac2577b4871afd0bb937291727041c6b6d
linhey <158179948@qq.com>
2018-03-08 19:00:36 +0800

    调整函数顺序

--- Sources/EmptyPage.swift
+++ Sources/EmptyPage.swift
@@ -11,33 +11,7 @@ public struct EmptyPage {
   
   private static var isRun = false
   
-  /// 交换方法
-  ///
-  /// - Parameters:
-  ///   - selector: 被交换的方法
-  ///   - replace: 用于交换的方法
-  ///   - classType: 所属类型
-  static func exchangeMethod(selector: Selector,
-                             replace: Selector,
-                             class classType: AnyClass) {
-    let select1 = selector
-    let select2 = replace
-    let select1Method = class_getInstanceMethod(classType, select1)
-    let select2Method = class_getInstanceMethod(classType, select2)
-    let didAddMethod  = class_addMethod(classType,
-                                        select1,
-                                        method_getImplementation(select2Method!),
-                                        method_getTypeEncoding(select2Method!))
-    if didAddMethod {
-      class_replaceMethod(classType,
-                          select2,
-                          method_getImplementation(select1Method!),
-                          method_getTypeEncoding(select1Method!))
-    }else {
-      method_exchangeImplementations(select1Method!, select2Method!)
-    }
-  }
-  
+  /// 起始函数
   public static func begin() {
     if isRun { return }
     isRun = true
@@ -97,6 +71,33 @@ public struct EmptyPage {
     exchangeMethod(selector: #selector(UICollectionView.deleteSections(_:)),
                    replace: #selector(UICollectionView.coll_emptyDeleteSections(_:)),
                    class: UICollectionView.self)
+  }
+  
+  /// 交换方法
+  ///
+  /// - Parameters:
+  ///   - selector: 被交换的方法
+  ///   - replace: 用于交换的方法
+  ///   - classType: 所属类型
+  static func exchangeMethod(selector: Selector,
+                             replace: Selector,
+                             class classType: AnyClass) {
+    let select1 = selector
+    let select2 = replace
+    let select1Method = class_getInstanceMethod(classType, select1)
+    let select2Method = class_getInstanceMethod(classType, select2)
+    let didAddMethod  = class_addMethod(classType,
+                                        select1,
+                                        method_getImplementation(select2Method!),
+                                        method_getTypeEncoding(select2Method!))
+    if didAddMethod {
+      class_replaceMethod(classType,
+                          select2,
+                          method_getImplementation(select1Method!),
+                          method_getTypeEncoding(select1Method!))
+    }else {
+      method_exchangeImplementations(select1Method!, select2Method!)
+    }
   }
 }
 
