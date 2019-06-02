46ca4ec783b0b797f0761534250a3bb77d3ad59f
linhey <158179948@qq.com>
2019-06-03 00:12:14 +0800

    fix: update demo

--- Example/EmptyPage/Demos/DemoCollectionViewController.swift
+++ Example/EmptyPage/Demos/DemoCollectionViewController.swift
@@ -8,72 +8,75 @@
 
 import UIKit
 import EmptyPage
+import SVProgressHUD
 
 private let reuseIdentifier = "Cell"
 
 class DemoCollectionViewController: UIViewController,UICollectionViewDelegate,UICollectionViewDataSource {
 
-  let collectionView = UICollectionView(frame: .zero, collectionViewLayout: UICollectionViewFlowLayout())
+    let collectionView = UICollectionView(frame: .zero, collectionViewLayout: UICollectionViewFlowLayout())
 
-  var rows = 0
+    var rows = 0
 
 
-  override func viewDidLoad() {
-    super.viewDidLoad()
-    view.backgroundColor = .white
-    view.addSubview(collectionView)
-    let layout = UICollectionViewFlowLayout()
-    layout.itemSize = CGSize(width: 100, height: 100)
-    layout.scrollDirection = .vertical
-    collectionView.backgroundColor = UIColor.gray
-    collectionView.frame = view.bounds
-    collectionView.delegate = self
-    collectionView.dataSource = self
-    collectionView.collectionViewLayout = layout
-    collectionView.ep.setLoading(EmptyStore.loading)
-    collectionView.ep.setEmpty(EmptyStore.custom(block1: event1(), block2: event2()))
-    self.collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: reuseIdentifier)
-    getData()
-  }
-
-  func event1() -> (() -> ()) {
-    return { self.getData() }
-  }
-  
-  func event2() -> (() -> ()) {
-    return {
-      self.rows = 2
-      self.collectionView.insertItems(at: [IndexPath(row: 0, section: 0),IndexPath(row: 1, section: 0)])
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+        view.addSubview(collectionView)
+        let layout = UICollectionViewFlowLayout()
+        layout.itemSize = CGSize(width: 100, height: 100)
+        layout.scrollDirection = .vertical
+        collectionView.backgroundColor = UIColor.gray
+        collectionView.frame = view.bounds
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        collectionView.collectionViewLayout = layout
+        collectionView.ep.setFirstLoading(UIView())
+        collectionView.ep.setEmpty(EmptyStore.custom(block1: event1(), block2: event2()))
+        self.collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: reuseIdentifier)
+        getData()
     }
-  }
-  
-  func getData() {
-    rows = 0
-    sleep(3) {[weak self] in
-      guard let base = self else { return }
-      base.collectionView.reloadData()
+
+    func event1() -> (() -> ()) {
+        return { self.getData() }
     }
-  }
+
+    func event2() -> (() -> ()) {
+        return {
+            self.rows = 2
+            self.collectionView.insertItems(at: [IndexPath(row: 0, section: 0),IndexPath(row: 1, section: 0)])
+        }
+    }
+
+    func getData() {
+        SVProgressHUD.show()
+        rows = 0
+        sleep(3) {[weak self] in
+            guard let base = self else { return }
+            SVProgressHUD.dismiss()
+            base.collectionView.reloadData()
+        }
+    }
 
 
-  func numberOfSections(in collectionView: UICollectionView) -> Int {
-    return 1
-  }
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
 
 
-  func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-    return rows
-  }
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return rows
+    }
 
-  func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-    let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath)
-    cell.backgroundColor = .blue
-    return cell
-  }
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath)
+        cell.backgroundColor = .blue
+        return cell
+    }
 
-  func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-    rows -= 1
-    collectionView.deleteItems(at: [indexPath])
-  }
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        rows -= 1
+        collectionView.deleteItems(at: [indexPath])
+    }
 
 }
