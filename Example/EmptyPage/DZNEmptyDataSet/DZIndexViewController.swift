de2d999b8b44073a341ee2a643ee5b4cdc0b1a85
林翰 <linhan@dxy.cn>
2020-04-09 15:14:46 +0800

    chore: demo 修改

--- Example/EmptyPage/DZNEmptyDataSet/DZIndexViewController.swift
+++ Example/EmptyPage/DZNEmptyDataSet/DZIndexViewController.swift
@@ -9,6 +9,7 @@
 import UIKit
 import EmptyPage
 import SnapKit
+import Stem
 
 // MARK: - 初始化
 extension UIImage{
@@ -97,10 +98,10 @@ class DZIndexViewController: UITableViewController {
                 return EmptyPageView.Template.standard
                     .set(image: UIImage(named: "placeholder_500px"))
                     .set(title: "No Photos",
-                         color: UIColor(hex: "#545454"),
+                         color: UIColor("#545454"),
                          font: UIFont.boldSystemFont(ofSize: 17))
                     .set(text: "Get started by uploading a photo.",
-                         color: UIColor(hex: "545454"),
+                         color: UIColor("#545454"),
                          font: UIFont.boldSystemFont(ofSize: 15))
                     .change(vspace: .titleWithText, value: 15)
                     .mix({ (item) in
@@ -116,14 +117,14 @@ class DZIndexViewController: UITableViewController {
                 return EmptyPageView.Template.standard
                     .set(image: UIImage(named: "placeholder_airbnb"))
                     .set(title: "No Messages",
-                         color: UIColor(hex: "#c9c9c9"),
+                         color: UIColor("#c9c9c9"),
                          font: UIFont(name: "HelveticaNeue-Light", size: 22)!)
                     .set(text: "When you have messages, you’ll see them here.",
-                         color: UIColor(hex: "#cfcfcf"),
+                         color: UIColor("#cfcfcf"),
                          font: UIFont.boldSystemFont(ofSize: 13))
                     .config(button: { (item) in
                         item.setTitle("Start Browsing", for: .normal)
-                        item.setTitleColor(UIColor(hex: "#6bceff"), for: .normal)
+                        item.setTitleColor(UIColor("#6bceff"), for: .normal)
                         item.titleLabel?.font = UIFont.boldSystemFont(ofSize: 16)
                         item.backgroundColor = UIColor.white
                     })
@@ -137,7 +138,7 @@ class DZIndexViewController: UITableViewController {
                 return EmptyPageView.Template.standard
                     .set(image: UIImage(named: "placeholder_appstore"))
                     .set(text: "There are no results for “wwdc2014”.",
-                         color: UIColor(hex: "#333333"),
+                         color: UIColor("#333333"),
                          font: UIFont.boldSystemFont(ofSize: 14))
                     .change(vspace: .imageWithTitle, value: 34)
                     .mix()
@@ -145,10 +146,10 @@ class DZIndexViewController: UITableViewController {
             case .camera:
                 return EmptyPageView.Template.standard
                     .set(title: "Please Allow Photo Access",
-                         color: UIColor(hex: "#5f6978"),
+                         color: UIColor("#5f6978"),
                          font: UIFont.boldSystemFont(ofSize: 18))
                     .set(text: "This allows you to share photos from your library and save photos to your camera roll.",
-                         color: UIColor(hex: "#5f6978"),
+                         color: UIColor("#5f6978"),
                          font: UIFont.systemFont(ofSize: 14))
                     .set(buttonTitle: "Continue")
                     .set(tap: tapEvent())
@@ -158,43 +159,43 @@ class DZIndexViewController: UITableViewController {
                 return EmptyPageView.Template.standard
                     .set(image: UIImage(named: "placeholder_dropbox"))
                     .set(title: "Star Your Favorite Files",
-                         color: UIColor(hex: "#25282b"),
+                         color: UIColor("#25282b"),
                          font: UIFont.boldSystemFont(ofSize: 17))
                     .set(text: "Favorites are saved for offline access.",
-                         color: UIColor(hex: "#7b8994"),
+                         color: UIColor("#7b8994"),
                          font: UIFont.systemFont(ofSize: 14.5))
-                    .set(buttonTitle: "Learn more", color: UIColor(hex: "48a1ea"),for: UIControl.State.normal)
+                    .set(buttonTitle: "Learn more", color: UIColor("#48a1ea"),for: UIControl.State.normal)
                     .set(tap: tapEvent())
                     .mix()
-                    .set(backgroundColor: UIColor(hex: "#f0f3f5"))
+                    .set(backgroundColor: UIColor("#f0f3f5"))
                     .set(tap: backgroundTapEvent())
             case .fackbook:
                 return EmptyPageView.Template.standard
                     .set(image: UIImage(named: "placeholder_facebook"))
                     .set(title: "No friends to show.",
-                         color: UIColor(hex: "#acafbd"),
+                         color: UIColor("#acafbd"),
                          font: UIFont.boldSystemFont(ofSize: 22))
                     .change(vspace: .imageWithTitle, value: 30)
                     .mix()
-                    .set(backgroundColor: UIColor(hex: "#eceef7"))
+                    .set(backgroundColor: UIColor("#eceef7"))
                     .set(tap: backgroundTapEvent())
             case .fancy:
                 return EmptyPageView.Template.standard
                     .set(image: UIImage(named: "placeholder_fancy"))
                     .set(title: "No Owns yet",
-                         color: UIColor(hex: "#494c53"),
+                         color: UIColor("#494c53"),
                          font: UIFont.boldSystemFont(ofSize: 14))
                     .set(text: "Tap Add to List and add things to Owns",
-                         color: UIColor(hex: "#7a7d83"),
+                         color: UIColor("#7a7d83"),
                          font: UIFont.boldSystemFont(ofSize: 13))
                     .mix()
-                    .set(backgroundColor: UIColor(hex: "#f0f0f0"))
+                    .set(backgroundColor: UIColor("#f0f0f0"))
                     .set(tap: backgroundTapEvent())
             case .foursquare:
                 return EmptyPageView.Template.standard
                     .set(image: UIImage(named: "placeholder_foursquare"))
                     .set(text: "Nobody has liked or commented on your check-ins yet.",
-                         color: UIColor(hex: "#cecbc6"),
+                         color: UIColor("#cecbc6"),
                          font: UIFont.systemFont(ofSize: 14))
                     .change(height: .button, value: 36)
                     .change(hspace: .text, value : 15)
@@ -202,7 +203,7 @@ class DZIndexViewController: UITableViewController {
                     .change(vspace: .imageWithTitle, value: 15)
                     .config(button: { (item) in
                         item.setTitle("Add friends to get started!", for: .normal)
-                        item.setTitleColor(UIColor(hex: "#00aeef"), for: .normal)
+                        item.setTitleColor(UIColor("#00aeef"), for: .normal)
                         item.layer.cornerRadius = 18
                         item.layer.borderColor = item.titleColor(for: .normal)!.cgColor
                         item.layer.borderWidth = 1
@@ -210,15 +211,15 @@ class DZIndexViewController: UITableViewController {
                     })
                     .set(tap: tapEvent())
                     .mix()
-                    .set(backgroundColor: UIColor(hex: "#fcfcfa"))
+                    .set(backgroundColor: UIColor("#fcfcfa"))
                     .set(tap: backgroundTapEvent())
             case .icloud:
                 return EmptyPageView.Template.standard
                     .set(title: "iCloud Photo Sharing",
-                         color: UIColor(hex: "#999999"),
+                         color: UIColor("#999999"),
                          font: UIFont.systemFont(ofSize: 30))
                     .set(text: "Share photos and videos with just the people you choose, and let them add photos, videos, and comments.",
-                         color: UIColor(hex: "#999999"),
+                         color: UIColor("#999999"),
                          font: UIFont.systemFont(ofSize: 18))
                     .change(hspace: .button, value: 100)
                     .change(hspace: .text, value: 40)
@@ -226,7 +227,7 @@ class DZIndexViewController: UITableViewController {
                     .change(vspace: .textWithButton, value: 20)
                     .config(button: { (item) in
                         item.setTitle("Create New Stream", for: .normal)
-                        item.setTitleColor(UIColor(hex: "#999999"), for: .normal)
+                        item.setTitleColor(UIColor("#999999"), for: .normal)
                         item.titleLabel?.font = UIFont.systemFont(ofSize: 14)
                         item.layer.cornerRadius = 2
                         item.layer.borderColor = item.titleColor(for: .normal)!.cgColor
@@ -240,10 +241,10 @@ class DZIndexViewController: UITableViewController {
                 return EmptyPageView.Template.standard
                     .set(image: UIImage(named: "placeholder_instagram"))
                     .set(title: "Instagram Direct",
-                         color: UIColor(hex: "#444444"),
+                         color: UIColor("#444444"),
                          font: UIFont(name: "HelveticaNeue-Light", size: 26)!)
                     .set(text: "Send photos and videos directly to your friends. Only the people you send to can see these posts.",
-                         color: UIColor(hex: "#444444"),
+                         color: UIColor("#444444"),
                          font: UIFont.systemFont(ofSize: 16))
                     .change(vspace: .imageWithTitle, value: 24)
                     .change(vspace: .titleWithText, value: 24)
@@ -270,10 +271,10 @@ class DZIndexViewController: UITableViewController {
                 return EmptyPageView.Template.standard
                     .set(image: UIImage(named: "placeholder_kickstarter"))
                     .set(title: "Activity empty",
-                         color: UIColor(hex: "#828587"),
+                         color: UIColor("#828587"),
                          font: UIFont.systemFont(ofSize: 16))
                     .set(text: "When you back a project or follow a friend, their activity will show up here.",
-                         color: UIColor(hex: "#828587"),
+                         color: UIColor("#828587"),
                          font: UIFont.systemFont(ofSize: 14))
                     .config(button: { (item) in
                         item.setTitle("Discover projects", for: .normal)
@@ -291,7 +292,7 @@ class DZIndexViewController: UITableViewController {
                     .change(hspace: .button, value: 40)
                     .change(height: .button, value: 45)
                     .mix()
-                    .set(backgroundColor: UIColor(hex: "#f7fafa"))
+                    .set(backgroundColor: UIColor("#f7fafa"))
                     .set(tap: backgroundTapEvent())
             case .path:
                 return EmptyPageView.Template.standard
@@ -300,20 +301,20 @@ class DZIndexViewController: UITableViewController {
                          color: UIColor.white,
                          font: UIFont.boldSystemFont(ofSize: 16))
                     .set(text: "Send a message or create a group.",
-                         color: UIColor(hex: "#a6978d"),
+                         color: UIColor("#a6978d"),
                          font: UIFont.systemFont(ofSize: 14))
                     .change(vspace: .imageWithTitle, value: 1)
                     .change(vspace: .titleWithText, value: 1)
                     .mix()
-                    .set(backgroundColor: UIColor(hex: "#726d67"))
+                    .set(backgroundColor: UIColor("#726d67"))
                     .set(tap: backgroundTapEvent())
             case .photo:
                 return EmptyPageView.Template.standard
                     .set(title: "No Photos or Videos",
-                         color: UIColor(hex: "#999999"),
+                         color: UIColor("#999999"),
                          font: UIFont.systemFont(ofSize: 30))
                     .set(text: "You can sync photos and videos onto your iPhone using iTunes.",
-                         color: UIColor(hex: "#999999"),
+                         color: UIColor("#999999"),
                          font: UIFont.systemFont(ofSize: 18))
                     .change(hspace: .text, value: 40)
                     .change(vspace: .titleWithText, value: 15)
@@ -322,10 +323,10 @@ class DZIndexViewController: UITableViewController {
             case .podcasts:
                 return EmptyPageView.Template.standard
                     .set(title: "No Podcasts",
-                         color: UIColor(hex: "#999999"),
+                         color: UIColor("#999999"),
                          font: UIFont.systemFont(ofSize: 30))
                     .set(text: "You can subscribe to podcasts in Top Charts or Featured.",
-                         color: UIColor(hex: "#999999"),
+                         color: UIColor("#999999"),
                          font: UIFont.systemFont(ofSize: 18))
                     .change(hspace: .text, value: 35)
                     .change(vspace: .titleWithText, value: 35)
@@ -335,10 +336,10 @@ class DZIndexViewController: UITableViewController {
                 return EmptyPageView.Template.standard
                     .set(image: UIImage(named: "placeholder_remote"))
                     .set(title: "Cannot Connect to a Local Network",
-                         color: UIColor(hex: "#555555"),
+                         color: UIColor("#555555"),
                          font: UIFont(name: "HelveticaNeue-Medium", size: 18)!)
                     .set(text: "You must connect to a Wi-Fi network to control iTunes or Apple TV",
-                         color: UIColor(hex: "#555555"),
+                         color: UIColor("#555555"),
                          font: UIFont(name: "HelveticaNeue-Medium", size: 11.75)!)
                     .change(hspace: .text, value: 20)
                     .mix()
@@ -346,7 +347,7 @@ class DZIndexViewController: UITableViewController {
             case .safari:
                 return EmptyPageView.Template.text
                     .set(text: "Safari cannot open the page because your iPhone is not connected to the Internet.",
-                         color: UIColor(hex: "#7d7f7f"),
+                         color: UIColor("#7d7f7f"),
                          font: UIFont.systemFont(ofSize: 18))
                     .change(hspace: .text, value: 20)
                     .mix()
@@ -354,10 +355,10 @@ class DZIndexViewController: UITableViewController {
             case .skype:
                 let attr = NSMutableAttributedString(string: "Keep all your favorite people together",
                                                      attributes: [.font : UIFont(name: "HelveticaNeue-Light", size: 17.75)!,
-                                                                  .foregroundColor: UIColor(hex: "#a6c3d1")])
+                                                                  .foregroundColor: UIColor("#a6c3d1")])
                 let attr2 = NSAttributedString(string: "\nadd favorites",
                                                attributes: [.font : UIFont(name: "HelveticaNeue-Light", size: 17.75)!,
-                                                            .foregroundColor : UIColor(hex: "00adf1")])
+                                                            .foregroundColor : UIColor("#00adf1")])
                 attr.append(attr2)
                 return EmptyPageView.Template.text
                     .set(attributed: attr)
@@ -368,7 +369,7 @@ class DZIndexViewController: UITableViewController {
                 return EmptyPageView.Template.standard
                     .set(image: UIImage(named: "placeholder_slack"))
                     .set(title: "You don't have any recent mentions",
-                         color: UIColor(hex: "#d7d7d7"),
+                         color: UIColor("#d7d7d7"),
                          font: UIFont.systemFont(ofSize: 19))
                     .change(hspace: .text, value: 20)
                     .mix()
@@ -377,16 +378,16 @@ class DZIndexViewController: UITableViewController {
                 return EmptyPageView.Template.standard
                     .set(image: UIImage(named: "placeholder_tumblr"))
                     .set(title: "This is your Dashboard.",
-                         color: UIColor(hex: "#aab6c4"),
+                         color: UIColor("#aab6c4"),
                          font: UIFont.systemFont(ofSize: 18))
                     .set(text: "When you follow some blogs, their latest posts will show up here!",
-                         color: UIColor(hex: "#828e9c"),
+                         color: UIColor("#828e9c"),
                          font: UIFont.systemFont(ofSize: 17))
                     .change(hspace: .text, value: 20)
                     .change(vspace: .imageWithTitle, value: 10)
                     .change(vspace: .titleWithText, value: 10)
                     .mix()
-                    .set(backgroundColor: UIColor(hex: "#34465c"))
+                    .set(backgroundColor: UIColor("#34465c"))
                     .set(tap: backgroundTapEvent())
             }
 
--- Example/Pods/Target Support Files/Pods-EmptyPage_Example/Pods-EmptyPage_Example-acknowledgements.markdown
+++ Example/Pods/Target Support Files/Pods-EmptyPage_Example/Pods-EmptyPage_Example-acknowledgements.markdown
@@ -206,32 +206,6 @@ This application makes use of the following third party libraries:
    limitations under the License.
 
 
-## Hue
-
-Licensed under the **MIT** license
-
-> Copyright (c) 2015 Hyper Interaktiv AS
->
-> Permission is hereby granted, free of charge, to any person obtaining
-> a copy of this software and associated documentation files (the
-> "Software"), to deal in the Software without restriction, including
-> without limitation the rights to use, copy, modify, merge, publish,
-> distribute, sublicense, and/or sell copies of the Software, and to
-> permit persons to whom the Software is furnished to do so, subject to
-> the following conditions:
->
-> The above copyright notice and this permission notice shall be
-> included in all copies or substantial portions of the Software.
->
-> THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
-> EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
-> MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
-> IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
-> CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
-> TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
-> SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-
-
 ## SVProgressHUD
 
 MIT License
@@ -282,7 +256,9 @@ THE SOFTWARE.
 
 ## Stem
 
-Copyright (c) 2019 linhay <is.linhay@outlook.com>
+MIT License
+
+Copyright (c) 2020 linhey
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
@@ -291,21 +267,23 @@ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 copies of the Software, and to permit persons to whom the Software is
 furnished to do so, subject to the following conditions:
 
-The above copyright notice and this permission notice shall be included in
-all copies or substantial portions of the Software.
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
 
 THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
 AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
 LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-THE SOFTWARE.
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
 
 
-## Stone
+## Stuart
 
-Copyright (c) 2019 linhey <158179948@qq.com>
+MIT License
+
+Copyright (c) 2020 linhey
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
@@ -314,16 +292,16 @@ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 copies of the Software, and to permit persons to whom the Software is
 furnished to do so, subject to the following conditions:
 
-The above copyright notice and this permission notice shall be included in
-all copies or substantial portions of the Software.
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
 
 THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
 AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
 LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-THE SOFTWARE.
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
 
 
 ## SwiftLint
