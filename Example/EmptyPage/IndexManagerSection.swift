aa286f84d2ba1b451385a7a01eb6a3ac3faad186
linhey <158179948@qq.com>
2020-07-01 17:21:41 +0800

    chore: 代码优化

--- Example/EmptyPage/IndexManagerSection.swift
+++ Example/EmptyPage/IndexManagerSection.swift
@@ -12,9 +12,9 @@ import Stem
 import EmptyPage
 
 class NetworkManager: EmptyPageCollectionViewManager {
-
+    
     var noNetwork: Bool = false
-
+    
     override func set(emptyView: (() -> UIView?)?) {
         super.set(emptyView: { [weak self] in
             guard let self = self else {
@@ -28,24 +28,24 @@ class NetworkManager: EmptyPageCollectionViewManager {
             return emptyView?()
         })
     }
-
+    
 }
 
 class IndexManagerSection: SectionCollectionProtocol {
-
+    
     var core: SectionCore?
-
+    
     var itemCount: Int = 3
-
+    
     func config(sectionView: UICollectionView) {
         sectionView.st.register(IndexTextCell.self)
         sectionView.st.register(IndexHeaderView.self, for: .header)
     }
-
+    
     var headerSize: CGSize {
         return IndexHeaderView.preferredSize(collectionView: sectionView, model: nil)
     }
-
+    
     var headerView: UICollectionReusableView? {
         let view = dequeue(kind: .header) as IndexHeaderView
         view.config("Manager")
@@ -55,11 +55,11 @@ class IndexManagerSection: SectionCollectionProtocol {
         }
         return view
     }
-
+    
     func itemSize(at row: Int) -> CGSize {
         return IndexTextCell.preferredSize(collectionView: sectionView, model: nil)
     }
-
+    
     func item(at row: Int) -> UICollectionViewCell {
         let cell: IndexTextCell = dequeue(at: row)
         switch row {
@@ -70,45 +70,15 @@ class IndexManagerSection: SectionCollectionProtocol {
         }
         return cell
     }
-
-    func setEmptyPage(contentView: SectionCollectionView) {
-        contentView.ep.set(emptyView: EmptyPageView.Template.image
-            .set(image: UIImage(color: .red, size: .init(width: 60, height: 60)))
-            .mix())
-    }
-
-    func enableNetworkItems() -> [ToolItem<SectionCollectionView>] {
-        var toolItems = [ToolItem<SectionCollectionView>]()
-        do {
-            let item = ToolItem<SectionCollectionView>(title: "开启网络")
-            item.action.delegate(on: self) { (self, contentView) in
-                let manager = contentView.ep.manager as? NetworkManager
-                manager?.noNetwork = false
-                contentView.ep.reload()
-            }
-            toolItems.append(item)
-        }
-
-        do {
-            let item = ToolItem<SectionCollectionView>(title: "关闭网络")
-            item.action.delegate(on: self) { (self, contentView) in
-                let manager = contentView.ep.manager as? NetworkManager
-                manager?.noNetwork = true
-                contentView.ep.reload()
-            }
-            toolItems.append(item)
-        }
-        return toolItems
-    }
-
+    
     func didSelectItem(at row: Int) {
         let contentView = SectionCollectionView()
         contentView.backgroundColor = .white
-
+        
         var toolItems = [ToolItem<SectionCollectionView>]()
         let vc  = TestViewController(contentView: contentView)
         sectionView.st.viewController?.st.push(vc: vc)
-
+        
         switch row {
         case 0:
             contentView.ep.set(manager: NetworkManager(delegate: contentView))
@@ -123,17 +93,51 @@ class IndexManagerSection: SectionCollectionProtocol {
                 item.canSelect = false
                 vc?.config(toolItems: [item] + self.enableNetworkItems())
             }
-
+            
             vc.onDisAppear.delegate(on: self) { (self, _) in
                 EmptyPageConfig.shared.setGloalManager(collectionView: nil)
             }
-
+            
             toolItems = [item]
         default:
             break
         }
-
+        
         vc.config(toolItems: toolItems)
     }
+    
+}
 
+extension IndexManagerSection {
+    
+    func setEmptyPage(contentView: SectionCollectionView) {
+        contentView.ep.set(emptyView: EmptyPageView.Template.image
+            .set(image: UIImage(color: .red, size: .init(width: 60, height: 60)))
+            .mix())
+    }
+    
+    func enableNetworkItems() -> [ToolItem<SectionCollectionView>] {
+        var toolItems = [ToolItem<SectionCollectionView>]()
+        do {
+            let item = ToolItem<SectionCollectionView>(title: "开启网络")
+            item.action.delegate(on: self) { (self, contentView) in
+                let manager = contentView.ep.manager as? NetworkManager
+                manager?.noNetwork = false
+                contentView.ep.reload()
+            }
+            toolItems.append(item)
+        }
+        
+        do {
+            let item = ToolItem<SectionCollectionView>(title: "关闭网络")
+            item.action.delegate(on: self) { (self, contentView) in
+                let manager = contentView.ep.manager as? NetworkManager
+                manager?.noNetwork = true
+                contentView.ep.reload()
+            }
+            toolItems.append(item)
+        }
+        return toolItems
+    }
+    
 }
