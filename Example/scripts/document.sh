460ea53c83c17883073128706760a3d8a8036841
linhey <158179948@qq.com>
2020-07-24 01:14:08 +0800

    chore: 添加 API 文档

--- Example/scripts/document.sh
+++ Example/scripts/document.sh
@@ -2,6 +2,6 @@
 file="EmptyPage"
 # 文档生成
 jazzy \
---podspec ../${file}.podspec \
---output ../document/
---min-acl [public, open]
+--podspec ../../${file}.podspec \
+--output ../../document/ \
+--theme fullwidth
