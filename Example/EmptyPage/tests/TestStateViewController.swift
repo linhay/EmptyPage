d691989e91d81d0f4221f4dc245de66bbc2cd97d
linhey <158179948@qq.com>
2020-09-09 01:08:25 +0800

    chore: 示例修改

--- Example/EmptyPage/tests/TestStateViewController.swift
+++ Example/EmptyPage/tests/TestStateViewController.swift
@@ -12,27 +12,31 @@ import MJRefresh
 import EmptyPage
 
 class TestStateViewController: UIViewController {
-
+    
     @IBOutlet weak var sectionView: SectionCollectionView!
+    @IBOutlet var stateButtons: [UIButton]!
+    
     lazy var manager = SectionCollectionManager(sectionView: sectionView)
     var token: NSKeyValueObservation?
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         sectionView.ex.set(emptyView: TemplateSet.image_animate().mix(), for: .loading)
         sectionView.ex.set(emptyView: TemplateSet.image_normal().mix(), for: .normal)
         sectionView.ex.set(emptyView: TemplateSet.standard1(tapEvent: nil).mix(), for: .noNetwork)
-
+        
         sectionView.ex.set(canScroll: false, for: .loading)
         sectionView.ex.set(canScroll: true, for: .normal)
         sectionView.ex.set(canScroll: false, for: .noNetwork)
-
-        token = sectionView.observe(\.bounds, options: [.new, .old]) { (_, _) in
-            print(self.sectionView.frame)
-        }
-
+        
         sectionView.ep.setEmpty(TemplateSet.standard1(tapEvent: nil))
-
+        
+        stateButtons[0].addTarget(self, action: #selector(setLoading(_:)), for: .touchUpInside)
+        stateButtons[1].addTarget(self, action: #selector(setNormal(_:)), for: .touchUpInside)
+        stateButtons[2].addTarget(self, action: #selector(setNoNetwork(_:)), for: .touchUpInside)
+        
+        setNormal(stateButtons[1])
+        
         sectionView.mj_header = MJRefreshNormalHeader(refreshingBlock: { [weak self] in
             Gcd.delay(.main, seconds: 2) { [weak self] in
                 guard let self = self else {
@@ -43,24 +47,30 @@ class TestStateViewController: UIViewController {
             }
         })
     }
-
+    
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         sectionView.mj_header?.beginRefreshing()
     }
-
-    @IBAction func setNoNetwork(_ sender: UIButton) {
+    
+    @objc
+    func setNoNetwork(_ sender: UIButton) {
+        stateButtons.forEach({ $0.backgroundColor = self.view.backgroundColor })
+        sender.backgroundColor = UIColor.lightGray
         sectionView.ex.change(state: .noNetwork)
-        manager.reload()
     }
-
-    @IBAction func setNormal(_ sender: UIButton) {
+    
+    @objc
+    func setNormal(_ sender: UIButton) {
+        stateButtons.forEach({ $0.backgroundColor = self.view.backgroundColor })
+        sender.backgroundColor = UIColor.lightGray
         sectionView.ex.change(state: .normal)
-        manager.reload()
     }
-
-    @IBAction func setLoading(_ sender: UIButton) {
+    
+    @objc
+    func setLoading(_ sender: UIButton) {
+        stateButtons.forEach({ $0.backgroundColor = self.view.backgroundColor })
+        sender.backgroundColor = UIColor.lightGray
         sectionView.ex.change(state: .loading)
-        manager.reload()
     }
 }
